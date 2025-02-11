c909387df8aef8ddb930e08fc09dc1e854632017
Mayur Jadhav <121611064+MjMayur@users.noreply.github.com>
2025-02-11 11:01:11 +0530

    Add files via upload

--- Maneger/screens/MainDrawer.js
+++ Maneger/screens/MainDrawer.js
@@ -1,6 +1,8 @@
 import { createDrawerNavigator } from "@react-navigation/drawer";
 import LogoutScreen from "./Logout";
 import HomeTabs from "./HomeTabs";
+import HelpScreen from "./HelpScreen";
+import AboutScreen from "./About";
 
 const Drawer = createDrawerNavigator();
 
@@ -8,8 +10,8 @@ export default function MainDrawer() {
   return (
     <Drawer.Navigator>
       <Drawer.Screen name="Home" component={HomeTabs} />
-      <Drawer.Screen name="Help" component={HomeTabs} />
-      <Drawer.Screen name="About" component={HomeTabs} />
+      <Drawer.Screen name="Help" component={HelpScreen} />
+      <Drawer.Screen name="About" component={AboutScreen} />
       <Drawer.Screen name="Logout" component={LogoutScreen} />
     </Drawer.Navigator>
   );
