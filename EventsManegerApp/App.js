85ee6b294f463d4669192a059b00b5b145522b8b
Mayur Jadhav <121611064+MjMayur@users.noreply.github.com>
2025-03-12 23:45:29 +0530

    Add files via upload

--- EventsManegerApp/App.js
+++ EventsManegerApp/App.js
@@ -1,7 +1,12 @@
 import "react-native-gesture-handler";
 import React from "react";
 import Navigation from "./screens/Navigation";
+import { MenuProvider } from "react-native-popup-menu";
 
 export default function App() {
-  return <Navigation />;
+  return (
+    <MenuProvider>
+      <Navigation />
+    </MenuProvider>
+  );
 }
